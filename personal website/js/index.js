f2868ba6f250ab39aa74fcb95b7fef72affbd3af
sophia502 <1848518984@qq.com>
2018-03-30 18:46:10 +0800

    个人网站：更改封面。

--- personal website/js/index.js
+++ personal website/js/index.js
@@ -1,7 +1,10 @@
 var oButton = document.getElementById('n-btn');
 var oBignav = document.getElementById('big-nav');
 var oSnav = document.getElementById('s-nav');
+var oUlnav = document.getElementById('ul-nav');
+var aliba = oUlnav.getElementsByClassName('b-a');
 var aCmenu = oBignav.getElementsByTagName('div');
+var oLine = document.getElementById('a-line');
 oButton.onclick = function () {
     for(var i = 0;i <aCmenu.length;i++){
     if(aCmenu[i].className=='nav-button cmenu'){
@@ -14,3 +17,11 @@ oButton.onclick = function () {
     }
 
 };
+for (var i=0;i<aliba.length;i++){
+    aliba[i].onmouseover = function () {
+        oLine.style.transform = 'rotate(45deg)';
+    };
+    aliba[i].onmouseout = function () {
+        oLine.style.transform = 'rotate(-45deg)';
+    };
+}
